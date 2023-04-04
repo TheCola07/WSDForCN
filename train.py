85c6713b19ea47250c41b4915855cb483b0995cd
TheLastCola <127419408+TheCola07@users.noreply.github.com>
2023-04-04 14:19:09 +0800

    Update train.py

--- train.py
+++ train.py
@@ -16,9 +16,9 @@ logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s',level=loggi
 logger.info("running %s" % ' '.join(sys.argv))
 
 # inp为输入语料, outp1为输出模型, outp2为vector格式的模型
-inp = 'E:\pyLearn\WSD\Data\corpusSegDone_1.txt'
-out_model = 'E:\pyLearn\WSD\Data\corpusSegDone_1.model'
-out_vector = 'E:\pyLearn\WSD\Data\corpusSegDone_1.vector'
+inp = '\\WSDForCN\data\corpusSegDone_1.txt'
+out_model = '\\WSDForCN\data\corpusSegDone_1.model'
+out_vector = '\\WSDForCN\data\corpusSegDone_1.vector'
  
 # 训练模型
 model = Word2Vec(LineSentence(inp), vector_size=50, window=5, min_count=5,workers=multiprocessing.cpu_count())
